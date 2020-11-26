4f12d91e82a8233ef54e74322bd4c1df3ff7b6f3
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-25 18:39:14 -0800

    Import and instantiate splide

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
+// import main css
 import './main.css';
+// end import main css
 
-// nav bar
+// import splide.js css
+import '@splidejs/splide/dist/css/splide.min.css';
+// end import splide.js css
+
+import Splide from '@splidejs/splide';
 import { navBarRun } from './app/navBar';
 
+// featured products
+new Splide('#splide1', {
+  perPage: 3,
+  breakpoints: {
+    768: {
+      perPage: 1,
+    },
+  },
+  rewind: true,
+  gap: '2em',
+}).mount();
+// end featured products
+
+// nav bar
 navBarRun();
 // end nav bar
