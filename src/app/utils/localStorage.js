5d96fc66faa15ad6a9b6a276223e32727dd9e42c
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-29 14:55:16 -0800

    Add storage class
    Add saveProducts and getProduct to save and get products from local storage on DOM load

---
+++ src/app/utils/localStorage.js
@@ -0,0 +1,13 @@
+export default class Storage {
+  // save products array to local storage
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  // Getting specific item from local storage based on unique id
+  static getProduct(id) {
+    const products = JSON.parse(localStorage.getItem('products'));
+    // returning the FIRST PRODUCT that matches unique id argument from the products array
+    return products.find((product) => product.id === id);
+  }
+}
