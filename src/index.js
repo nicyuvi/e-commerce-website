d3df44949de7b7d6ddde96b5053256dfbe1a33c6
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-28 23:06:54 -0800

    Remove splide import

--- src/index.js
+++ src/index.js
@@ -7,23 +7,9 @@ import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
 // end import splide.js css
 
 import 'regenerator-runtime/runtime';
-import Splide from '@splidejs/splide';
 import { navBarRun } from './app/navBar';
 // eslint-disable-next-line no-unused-vars
 import domLoad from './app/domLoad';
-
-// featured products
-new Splide('#splide1', {
-  perPage: 3,
-  breakpoints: {
-    768: {
-      perPage: 1,
-    },
-  },
-  rewind: true,
-  gap: '2em',
-}).mount();
-// end featured products
 
 // nav bar
 navBarRun();
