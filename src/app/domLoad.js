79f7811c224ca7911506723b3919aa45e337f766
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-29 14:52:46 -0800

    Call saveProducts

--- src/app/domLoad.js
+++ src/app/domLoad.js
@@ -1,16 +1,19 @@
 import getProducts from './utils/products';
 import UI from './userInterface';
 import ProductView from './productView';
-// event on page load
+import Storage from './utils/localStorage';
+
 export default document.addEventListener('DOMContentLoaded', () => {
   // get products from contentful
   getProducts()
     .then((products) => {
-      // call static method displayProducts
+      // display products to DOM
       UI.displayProducts(products);
+      // save products array to local storage
+      Storage.saveProducts(products);
     })
     .then(() => {
-      // call static method viewProduct
+      // display product in product view
       ProductView.viewProduct();
     })
     .catch((err) => {
