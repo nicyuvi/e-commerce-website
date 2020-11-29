66a7f08ca7085807eac55e8d138c94d32465cfe2
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-29 14:14:04 -0800

    Call viewProduct method

--- src/app/domLoad.js
+++ src/app/domLoad.js
@@ -1,10 +1,19 @@
 import getProducts from './utils/products';
 import UI from './userInterface';
+import ProductView from './productView';
 // event on page load
 export default document.addEventListener('DOMContentLoaded', () => {
   // get products from contentful
-  getProducts().then((products) => {
-    // call static method displayProducts
-    UI.displayProducts(products);
-  });
+  getProducts()
+    .then((products) => {
+      // call static method displayProducts
+      UI.displayProducts(products);
+    })
+    .then(() => {
+      // call static method viewProduct
+      ProductView.viewProduct();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
