90aa1d7db404abfdc2431566439b94e27827de2b
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-29 22:30:59 -0800

    Import and call preventLinkDefault

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
 // end import splide.js css
 
 import 'regenerator-runtime/runtime';
+import preventLinkDefault from './app/utils/preventLinkDefault';
 import { navBarRun } from './app/navBar';
 // eslint-disable-next-line no-unused-vars
 import domLoad from './app/domLoad';
 
-// nav bar
 navBarRun();
-// end nav bar
+preventLinkDefault();
