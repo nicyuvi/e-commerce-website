a5717131fc927a36292cccaf645889c128652eb0
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-11-30 18:49:25 -0800

    Change forEach loop to for loop
    This way I can display only the 9 latest products instead of all of them in this section

--- src/app/userInterface.js
+++ src/app/userInterface.js
@@ -4,34 +4,42 @@ const productsDOM = document.querySelector('#featured-products-cards');
 
 export default class UI {
   // Display products
-  static displayProducts(products) {
+  static displayFeaturedProducts(products) {
+    // declare empty card string
     let cards = '';
-    // loop through each product object in products array
-    products.forEach((product) => {
+
+    // loop through 9 latest products from contentful
+    for (let i = 0; i < 9; i += 1) {
+      // destructure each object in products array
+      // eslint-disable-next-line object-curly-newline
+      const { title, price, id, image } = products[i];
+
       // add product cards to empty cards string
       cards += `
-        <div data-id=${product.id} class="product-card cursor-pointer">
+        <div data-id=${id} class="product-card cursor-pointer">
           <div class="border border-gray-300 h-96  overflow-hidden">
-            <img class="object-center object-cover h-96 transform hover:scale-105 transition-transform duration-300" src="${product.image}"
+            <img class="object-center object-cover h-96 transform hover:scale-105 transition-transform duration-300" src="${image}"
               alt="image of featured product">
           </div>
-          <h2 class="text-2xl">${product.title}</h2>
-          <p class="text-xl text-gray-500">$${product.price}</p>
+          <h2 class="text-2xl">${title}</h2>
+          <p class="text-xl text-gray-500">$${price}</p>
       </div>`;
+
       // add slides to empty slides string
       const slides = `
           <div class="splide__slide__container border border-gray-300 h-96 md:h-72 overflow-hidden">
-            <img class="object-center object-cover h-96 md:h-72 transform hover:scale-105 transition-transform duration-300" src="${product.image}" alt="image of featured product">
+            <img class="object-center object-cover h-96 md:h-72 transform hover:scale-105 transition-transform duration-300" src="${image}" alt="image of featured product">
           </div>
-          <h2 class="text-2xl">${product.title}</h2>
-          <p class="text-xl text-gray-500">$${product.price}</p>
+          <h2 class="text-2xl">${title}</h2>
+          <p class="text-xl text-gray-500">$${price}</p>
       `;
+
       // add slides to splide slider
       splide.add(
         // eslint-disable-next-line comma-dangle
-        `<li data-id=${product.id} class="splide__slide cursor-pointer">${slides}</li>`
+        `<li data-id=${id} class="splide__slide cursor-pointer">${slides}</li>`
       );
-    });
+    }
     // add product cards to DOM
     productsDOM.innerHTML = cards;
   }
