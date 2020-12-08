75e852ca47acd8a323caae08a4bda6607e7902e4
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-12-07 16:52:26 -0800

    Optimize for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ['./public/**/*.html', './src/**/*.js'],
   theme: {},
   variants: {},
   plugins: [],
