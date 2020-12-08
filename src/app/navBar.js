847753acc80b34a9e99a7e6e754b753671046e76
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-12-08 12:14:26 -0800

    Update body scroll lock with media query

--- src/app/navBar.js
+++ src/app/navBar.js
@@ -11,6 +11,10 @@ const cartBtn = document.querySelectorAll('.cart-btn');
 const cartSidebar = document.querySelector('#cart-sidebar');
 const cartOverlay = document.querySelector('#cart-overlay');
 
+// Media query
+const mediaQuery = '(min-width: 640px)';
+const mediaQueryList = window.matchMedia(mediaQuery);
+
 function toggleNavSidebar() {
   // had to replace 'toggle' with 'add/remove' to make use of body scroll lock library
   if (navSidebar.classList.contains('-translate-x-full')) {
@@ -52,6 +56,17 @@ export function toggleCartSidebar() {
   }
 }
 
+// remove scroll lock from mobile to tablet view
+function removeScrollLock(e) {
+  if (e.matches) {
+    // when nav sidebar is hidden we enable body scroll
+    enableBodyScroll(navSidebar);
+  } else if (!navSidebar.classList.contains('-translate-x-full')) {
+    // we only disable body scroll if navsidebar is already open from tablet to mobile view
+    disableBodyScroll(navSidebar);
+  }
+}
+
 export default () => {
   // nav sidebar toggle
   hamBtn.addEventListener('click', () => {
@@ -77,5 +92,9 @@ export default () => {
       }
     });
   });
+  // close cart on overlay click
   cartOverlay.addEventListener('click', toggleCartSidebar);
+
+  // clear/enable body scroll lock from mobile to tablet view or vice versa
+  mediaQueryList.addEventListener('change', removeScrollLock);
 };
